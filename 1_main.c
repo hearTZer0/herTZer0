3e083f59d03e2eda62fadd8858efea3015e4a063
hearTZer0 <1718790056@qq.com>
2025-10-20 19:19:07 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,16 @@
-//
-//
-//
-#include <stdio.h>
+//202510306123
+//1718790056@qq.com
+//王天瑞
+
+#include<stdio.h>
+ 
 int main()
-{
-    printf("Hello World");
+{ 
+    float a = 0;
+    printf("输入摄氏度：");
+    scanf("%f", &a);
+    float b = a * 9 / 5 + 32;
+    printf("%.1f华氏度", b);
     return 0;
+
 }
