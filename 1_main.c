3420ad1054bbb315f00deca89a48238b414680a1
hearTZer0 <1718790056@qq.com>
2025-10-27 18:51:36 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -3,14 +3,44 @@
 //王天瑞
 
 #include<stdio.h>
- 
-int main()
-{ 
-    float a = 0;
-    printf("输入摄氏度：");
-    scanf("%f", &a);
-    float b = a * 9 / 5 + 32;
-    printf("%.1f华氏度", b);
-    return 0;
 
+int main()
+{
+    int n = 0;
+    int isPrime = 1;
+    
+    scanf("%d", &n);
+    
+    if(n <= 1 || n >= 50)
+    {
+        printf("错误");
+        return 1;
+    }
+
+    if(n == 2)
+    {
+        isPrime = 1;
+    }
+    else
+    {
+        for(int i = 2; i <= n/2; i++)
+        {
+            if(n % i == 0)
+            {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+
+    if(isPrime)
+    {
+        printf("密钥安全，密码设置成功\n");
+    }
+    else
+    {
+        printf("密钥不安全，请重新输入\n");
+    }
+    
+    return 0;
 }
