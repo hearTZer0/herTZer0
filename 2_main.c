f60911b130b37f7931e84d40798c5c70c8ada7f8
hearTZer0 <1718790056@qq.com>
2025-10-27 19:20:36 +0800

    Update 2_main.c

--- 2_main.c
+++ 2_main.c
@@ -2,46 +2,30 @@
 //1718790056@qq.com
 //王天瑞
 
-#include<stdio.h>
- 
-int main()
-{
- int score;   
- 
- printf("输入成绩:");
- 
- scanf("%d",&score);
- 
- if (score < 0 || score > 100)
- {
-    printf("错误\n");
-    return 1;
- }  
- 
- if (score >= 90 && score <= 100) 
- {
-    printf("A\n");
- }  
- 
- else if (score >= 80 && score <= 89 )
- {
-    printf("B\n");
- }
- 
- else if (score >= 70 && score <= 79)
- {
-    printf("C\n");
- }
- 
- else if(score >= 60 && score <= 69)
- {
-    printf("D\n");
- }
- 
- else if(score < 60)
- {
-    printf("E\n");
- }
+#include <stdio.h>
 
- return 0;
+int main() 
+{
+    int num, hundreds, tens, units;
+    int first = 1;
+
+    for (num = 100; num <= 999; num++) 
+    {
+        hundreds = num / 100;
+        tens = (num / 10) % 10;
+        units = num % 10;
+        
+        int sum = hundreds * hundreds * hundreds + tens * tens * tens + units * units * units;
+
+        if (sum == num) 
+        {
+            if (!first) 
+            {
+                printf(" ");
+            }
+            printf("%d", num);
+            first = 0;
+        }
+    }
+    return 0;
 }
